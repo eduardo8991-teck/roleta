da47924382b170207a7cedb9472c4e9e35727620
eduardo8991-teck <eduardo.8991@colegioprogresso.g12.br>
2026-03-30 12:56:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+const canvas = document.getElementById("roleta");
+const ctx = canvas.getContext("2d");
+
+const opcoes = ["Banner", "Cursor", "Ícones",
+ "Background"];
+
+
+     let giroTotal = 0;
+
+function desenhar() {
+  const total = opcoes.length;
+  const angulo = (2 * Math.PI) / total;
+
+  for (let i = 0; i < total; i++) {
+    ctx.beginPath();
+    ctx.moveTo(150, 150);
+
+    ctx.arc(
+      150, 150,   // centro
+      150,        // tamanho
+      angulo * i, // começo
+      angulo * (i + 1) // fim
+    );
+   
+if (i === 0) {
+  ctx.fillStyle = "rgb(0, 0, 0)";
+}
+
+if (i === 1) {
+  ctx.fillStyle = "rgba(255, 0, 0, 0.92)";
+}
+
+if (i === 2) {
+  ctx.fillStyle = "rgb(0, 255, 110)";
+}
+
+if (i === 3) {
+  ctx.fillStyle = "rgb(0, 140, 255)";
+}
+
+ctx.fill();
+
+    ctx.save();
+
+    ctx.translate(150, 150);
+    ctx.rotate(angulo * i + angulo / 2);
+
+   ctx.fillStyle = "white";
+ctx.font = "bold 18px Arial";
+ctx.textAlign = "center";
+ctx.textBaseline = "middle";
+    
+    ctx.fillText(opcoes[i], 80, 0);
+
+    ctx.restore();
+
+  }
+}
+
+desenhar();
+function girar() {
+  let giro = Math.random() * 360 + 720;
+
+  giroTotal = giroTotal + giro;
+
+  canvas.style.transform = "rotate(" + giroTotal + "deg)";
+
+  let sorteado = Math.floor(Math.random() * opcoes.length);
+
+  document.getElementById("resultado").innerText =
+    "Resultado: " + opcoes[sorteado];
+}
